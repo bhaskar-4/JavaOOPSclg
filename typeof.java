664587f6acdea0a9820f4473e13c0dfca85d33d1
bhaskar-4 <sharma.bhaskar1721@gmail.com>
2024-07-26 13:22:37 +0530

    Add files via upload

---
+++ typeof.java
@@ -0,0 +1,27 @@
+
+
+public class typeof {
+    public static void main(String[] args) {
+        
+    
+    int x=423;
+    System.out.println(x.getClass().getSimpleName());
+    double y=4.786;
+    System.out.println(y.getClass().getSimpleName());
+    char z='a';
+    System.out.println(z.getClass().getSimpleName());
+    long a=1098384739;
+    System.out.println(a.getClass().getSimpleName());
+    short b=4;
+    System.out.println(b.getClass().getSimpleName());
+    byte c= 10;
+    System.out.println(c.getClass().getSimpleName());
+    float d=4.6f;
+    System.out.println(d.getClass().getSimpleName());
+
+
+}
+
+
+    
+}
---
+++ variable.java
@@ -0,0 +1,22 @@
+
+
+public class variable {
+    public static void main(String[] args) {
+        int x=423;
+        System.out.println(x);
+        double y=4.786;
+        System.out.println(y);
+        char z='a';
+        System.out.println(z);
+        long a=1098384739;
+        System.out.println(a);
+        short b=4;
+        System.out.println(b);
+        byte c= 10;
+        System.out.println(c);
+        float d=4.6f;
+        System.out.println(d);
+
+}
+    
+}
